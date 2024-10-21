be012c98e907d57c4aac6302b31acd2b8404d874
masuda1121 <kenya.masuda1121@gmail.com>
2024-10-21 17:22:15 +0900

    第15回演習課題提出

--- src/main/java/raisetech/studentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/studentManagement/domain/StudentDetail.java
@@ -1,16 +1,20 @@
 package raisetech.studentManagement.domain;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import raisetech.studentManagement.data.Student;
-import raisetech.studentManagement.data.StudentsCourses;
+import raisetech.studentManagement.data.StudentCourse;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDetail {
 
   private Student student;
-  private List<StudentsCourses> studentsCourses;
+  private List<StudentCourse> studentCourseList;
 
 }
